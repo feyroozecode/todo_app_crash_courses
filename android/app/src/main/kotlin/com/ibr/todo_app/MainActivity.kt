2e0a52d373a1ef5b6a71dc2afe471aedabef07c3
ibrahmad <ibrahmad18@gmail.com>
2023-02-12 18:21:57 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/ibr/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ibr.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class PageAccueil extends StatefulWidget {
+  const PageAccueil({super.key});
+
+  @override
+  State<PageAccueil> createState() => _PageAccueilState();
+}
+
+class _PageAccueilState extends State<PageAccueil> {
+  var appTitle = "Mon program";
+
+  @override
+  Widget build(BuildContext context) {
+    // recuperer la taille de l'ecran
+    var size = MediaQuery.of(context).size;
+    double screenHeight = size.height;
+    double screenWidth = size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          appTitle,
+          style: const TextStyle(
+              color: Colors.white, fontSize: 25, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Icon(
+              Icons.info,
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
+      body: Container(
+        height: screenHeight,
+        width: screenWidth,
+        color: const Color.fromARGB(255, 235, 235, 235),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              "Bienvenue, ajouter une tache",
+              style: TextStyle(fontSize: 24, fontStyle: FontStyle.italic),
+            ),
+            MaterialButton(
+              color: Colors.white,
+              elevation: 3.0,
+              onPressed: () {},
+              child: const Text(
+                'Ajouter une tache',
+                style: TextStyle(color: Colors.orange),
+              ),
+            ),
+
+            const Divider(
+              height: 20,
+            ), 
+            // afficher des element sur une ligne 
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, // aficher au centre 
+              children: const [
+                Text('bonjour'), 
+                Icon(Icons.person)
+              ],
+            ), 
+          ],
+        )),
+        floatingActionButton: FloatingActionButton(onPressed: (){}, 
+        child: const Icon(Icons.add)),
+       //bottomNavigationBar: BottomNavigationBar(),
+    );
+  }
+}
